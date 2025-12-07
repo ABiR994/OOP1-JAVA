91e6b524d8af620c5b6fe2dc1cb5fa146b827960
ABiR994 <salemnurabir2@gmail.com>
2025-12-07 22:45:21 +0600

    Refactor StringDemo.java to improve output clarity and consistency

--- Midterm/StringDemo.java
+++ Midterm/StringDemo.java
@@ -8,33 +8,33 @@ public class StringDemo
 		String s2 = "Java";
 		String s3 = new String("Java");
 		String s4 = new String("Java");
-		
+
 		System.out.println(s1.length()); // 4
 		System.out.println(s1.isEmpty()); //false
 		System.out.println(s1.charAt(2));// v
-		
-		
+
+
 		if(s1 == s2)
 		{
-			System.out.println("Same"); // print same
+			System.out.println("Same"); // Same
 		}
 		else
 		{
 			System.out.println("Different");
 		}
-		
+
 		if(s3 == s4)
 		{
 			System.out.println("Same");
 		}
 		else
 		{
-			System.out.println("Different");// different
+			System.out.println("Different");// Different
 		}
-		
+
 		if(s1.equals(s2))
 		{
-			System.out.println("Same"); //same
+			System.out.println("Same"); // Same
 		}
 		else
 		{
@@ -42,55 +42,92 @@ public class StringDemo
 		}
 		if(s3.equals(s4))
 		{
-			System.out.println("Same"); // same
+			System.out.println("Same"); // Same
 		}
 		else
 		{
 			System.out.println("Different");
 		}
-		
-		
-		String s5 = new String(s1 + s3); 
+
+
+		if(s1 == s3)
+		{
+			System.out.println("Same");
+		}
+		else
+		{
+			System.out.println("Different"); // Different
+		}
+
+		if(s2 == s4)
+		{
+			System.out.println("Same");
+		}
+		else
+		{
+			System.out.println("Different");// Different
+		}
+
+		if(s1.equals(s3))
+		{
+			System.out.println("Same"); // Same
+		}
+		else
+		{
+			System.out.println("Different");
+		}
+		if(s2.equals(s4))
+		{
+			System.out.println("Same"); // Same
+		}
+		else
+		{
+			System.out.println("Different");
+		}
+
+
+		String s5 = new String(s1 + s3);
 		System.out.println(s5);  // JavaJava
 		String s6 = s1 + s3;
 		System.out.println(s6); // JavaJava
-		
+
 		System.out.println(s5.substring(2,5)); //vaJ
 		System.out.println(s5.substring(2,6)); //vaJa
-		
+
 		s1 = "Hello";
 		s1=s1.concat(s3);
 		System.out.println(s1);  // HelloJava
-		System.out.println(s3);
+		System.out.println(s3); // Java
 		s2 = "Good ";
 		s3= "Morning";
 		s2.concat(s3);
 		System.out.println(s2); // Good
-		
+
 		System.out.println(s2.concat(s3)); // Good Morning
-		
-		String s7 = "wOlvaRIne";  
+
+		String s7 = "wOlvaRIne";
 		String s8 = s7.toUpperCase();
+		String s9 = s7.toLowerCase();
 		System.out.println(s7); //wOlvaRIne
 		System.out.println(s8);  //WOLVARINE
-		
+		System.out.println(s9);  //wolvarine
+
 		String str10 = "Apple";
         String str11 = "Banana";
 
-        
-        int result = str10.compareTo(str11);
-        System.out.println("compareTo result: " + result); //-1
-        
-        int index = str11.indexOf('n'); 
+		System.out.println("compareTo result (str10.compareTo(str10)): " + str10.compareTo(str10)); //0
+        System.out.println("compareTo result (str10.compareTo(str11)): " + str10.compareTo(str11)); //-1
+		System.out.println("compareTo result (str11.compareTo(str10)): " + str11.compareTo(str10)); //1
+
+        int index = str11.indexOf('n');
         System.out.println("indexOf 'n' in Banana: " + index); //2
 
-        int indexStr = str11.indexOf("ana"); 
-        System.out.println("indexOf 'ana' in Banana: " + indexStr);
-        
+        int indexStr = str11.indexOf("ana");
+        System.out.println("indexOf 'ana' in Banana: " + indexStr); //1
 
-        
+
         String sub = str11.substring(1, 4);
         System.out.println("substring(1,4) of Banana: " + sub); //ana
-        
+
 	}
 }
